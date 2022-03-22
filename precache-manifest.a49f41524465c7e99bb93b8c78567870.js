54af4289d521565138ec77f78bfbd2d198b78173
Nikita Kalyaev <kalyaev_nv@resanta.ru>
2022-03-22 14:54:01 +0300

    Updates

---
+++ precache-manifest.a49f41524465c7e99bb93b8c78567870.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e617cc955ccd0cc12495e1e3278fbf40",
+    "url": "/WeatherForecast/index.html"
+  },
+  {
+    "revision": "a7295d466ea1fd7d3796",
+    "url": "/WeatherForecast/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "391216138ba69c8eabbb",
+    "url": "/WeatherForecast/static/css/main.99d6aa82.chunk.css"
+  },
+  {
+    "revision": "a7295d466ea1fd7d3796",
+    "url": "/WeatherForecast/static/js/2.1e124fa5.chunk.js"
+  },
+  {
+    "revision": "391216138ba69c8eabbb",
+    "url": "/WeatherForecast/static/js/main.7ce650d7.chunk.js"
+  },
+  {
+    "revision": "89f0cbc46a7162c1955c",
+    "url": "/WeatherForecast/static/js/runtime-main.503bc193.js"
+  }
+]);
